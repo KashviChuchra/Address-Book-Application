ae3a8f08a1a3c5e9a6415dfeae1f3d10095e07e1
KashviChuchra20 <kashvi0793.be23@chitkara.edu.in>
2026-09-13 18:43:14 +0530

    UC3/Add Contacts Feature added

--- src/AddressBookApp/Program.cs
+++ src/AddressBookApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AddressBookApp.src.AddressBookApp.Exceptions;
 using AddressBookApp.src.AddressBookApp.Models;
+using AddressBookApp.src.AddressBookApp.Services;
 using AddressBookApp.src.AddressBookApp.Validation;
 using System;
 using System.ComponentModel.DataAnnotations;
@@ -20,5 +21,68 @@ class Program
         {
             Console.WriteLine("Error: " + e.Message);
         }
+
+        // ---------------UC3-----------------------------------
+        AddressBook addressBook1 = new AddressBook();
+        while (true)
+        {
+            Console.WriteLine("1- Add Contact");
+            Console.WriteLine("2- Show All Contacts");
+            Console.WriteLine("0- Exit");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                try
+                {
+                    Console.Write("Enter first name:");
+                    string firstName = Console.ReadLine();
+
+                    Console.Write("Enter last name:");
+                    string lastName = Console.ReadLine();
+
+                    Console.Write("Enter address:");
+                    string address = Console.ReadLine();
+
+                    Console.Write("Enter city:");
+                    string city = Console.ReadLine();
+
+                    Console.Write("Enter state:");
+                    string state = Console.ReadLine();
+
+                    Console.Write("Enter zip:");
+                    string zip = Console.ReadLine();
+
+                    Console.Write("Enter phone number:");
+                    string phoneNumber = Console.ReadLine();
+
+                    Console.Write("Enter email:");
+                    string email = Console.ReadLine();
+
+                    Contact contact2 = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+                    validator.Validate(contact2);
+                    addressBook1.AddContact(contact2);
+                    Console.WriteLine("Contact Added Successfully!");
+                }
+                catch (InvalidContactException e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+
+            }
+            else if (choice == "2")
+            {
+                addressBook1.PrintAll();
+            }
+            else if (choice == "0")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Choice!");
+            }
+        }     
     }
 }
---
+++ src/AddressBookApp/Services/AddressBook.cs
@@ -0,0 +1,32 @@
+﻿using AddressBookApp.src.AddressBookApp.Models;
+using AddressBookApp.src.AddressBookApp.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookApp.src.AddressBookApp.Services
+{
+    internal class AddressBook
+    {
+        private List<Contact> contacts = new();
+        public IReadOnlyList<Contact> Contacts
+        {
+            get
+            {
+                return contacts;
+            }
+        }
+        public void AddContact(Contact c)
+        {
+            contacts.Add(c);
+       
+        }
+        public void PrintAll()
+        {
+            foreach(Contact contact in contacts)
+            {
+                Console.WriteLine(contact.ToString());
+            }
+        }
+    }
+}
