909430db4fe0d20b18b3037cf6cb04a77342c808
KashviChuchra20 <kashvi0793.be23@chitkara.edu.in>
2026-09-16 01:06:09 +0530

    Testing : basics of contact validation added

---
+++ tests/AddressBookTests/ContactValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using AddressBookApp.src.AddressBookApp.Exceptions;
+using AddressBookApp.src.AddressBookApp.Models;
+using AddressBookApp.src.AddressBookApp.Validation;
+
+namespace AddressBookTests
+{
+    public class ContactValidatorTests
+    {
+        private ContactValidator validator;
+        [SetUp]
+        public void Setup()
+        {
+            validator = new ContactValidator();
+        }
+
+        [Test]
+        public void ValidContact_ShouldPass()
+        {
+            Contact contact = new Contact("Kashvi", "Chuchra", "Gobind Colony, Dhand Road", "Kaithal", "Haryana", "136027", "7777555567", "kashvi@gmail.com");
+            Assert.DoesNotThrow(() => validator.Validate(contact));
+        }
+
+        [Test]
+        public void InValidContact_ShouldThrowError()
+        {
+            Contact contact = new Contact("Kashvi", "Chuchra", "Gobind Colony, Dhand Road", "Kaithal", "Haryana", "136027", "777755556", "kashvi@gmail.com");
+            Assert.Throws<InvalidContactException>(() => validator.Validate(contact));
+        }
+
+        [Test]
+        public void InvalidEmail()
+        {
+            Contact contact = new Contact("Kashvi", "Chuchra", "Gobind Colony, Dhand Road", "Kaithal", "Haryana", "136027", "777755556", "kashv@");
+            Assert.Throws<InvalidContactException>(() => validator.Validate(contact));
+        }
+    }
+}
